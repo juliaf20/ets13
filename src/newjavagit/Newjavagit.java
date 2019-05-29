6e83c7dec0b5e0d22ed8194e62750bc03647e54a
julia <julia@Julia>
2019-05-29 20:26:57 +0100

    El programa con la matriz funcionando, falta hacer un case para elegir matriz, y luego hacer el JOption Pane

--- src/newjavagit/Newjavagit.java
+++ src/newjavagit/Newjavagit.java
@@ -7,35 +7,70 @@ public class Newjavagit {
     public static void main(String[] args) {
         Newjavagit njg;
         njg = new Newjavagit();
-        njg.crearMariz();
-        njg.imprimir();
+        njg.crearMariz3x3();
+        njg.crearMariz4x4();
+        njg.crearMariz5x5();
+        //teclado = new Scanner(System.in);
     }
 
-    Scanner teclado;
+    //Scanner teclado;
     int[][] matriz;
-    
-    public void crearMariz() {
-        teclado = new Scanner(System.in);
-        System.out.print("Cuantas fila tiene la matriz:");
-        int filas = teclado.nextInt();
-        System.out.print("Cuantas columnas tiene la matriz:");
-        int columnas = teclado.nextInt();
-        matriz = new int[filas][columnas];
-        
-        for (int f = 0; f < matriz.length; f++) {
-            for (int c = 0; c < matriz[f].length; c++) {
-                System.out.print("Ingrese componente:");
-                matriz[f][c] = teclado.nextInt();
+
+    public void crearMariz3x3(/*int x, int y*/) {
+        System.out.println("Matriz de 3");
+        matriz = new int[4][4];
+        //teclado = new Scanner(System.in);
+        for (int f = 1; f < matriz.length; f++) {
+            for (int c = 1; c < matriz[f].length; c++) {
+                if (f == 1 || c == 1 || f == matriz.length - 1 || c == matriz[f].length - 1) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
             }
+            System.out.println("");
+        }
+    }/*
+        for (int f = 1; f <= x; f++) {
+            for (int c = 1; c <= y; c++) {
+                if (f == 1 || c == 1 || f == x || c == y) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println("");
+        }
+    }*/
+
+    public void crearMariz4x4() {
+        System.out.println("Matriz de 4");
+        matriz = new int[5][5];
+        //teclado = new Scanner(System.in);
+        for (int f = 1; f < matriz.length; f++) {
+            for (int c = 1; c < matriz[f].length; c++) {
+                if (f == 1 || c == 1 || f == matriz.length - 1 || c == matriz[f].length - 1) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println("");
         }
     }
-
-    public void imprimir() {
-        for(int f = 0; f < matriz.length; f++) {
-            for (int c = 0; c < matriz[f].length; c++) {
-                System.out.print(matriz[f][c] + " ");
+    
+     public void crearMariz5x5() {
+        System.out.println("Matriz de 5");
+        matriz = new int[6][6];
+        for (int f = 1; f < matriz.length; f++) {
+            for (int c = 1; c < matriz[f].length; c++) {
+                if (f == 1 || c == 1 || f == matriz.length - 1 || c == matriz[f].length - 1) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 
