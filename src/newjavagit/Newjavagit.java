0077f82330592542f3ed2a8d4b4e42742fac32ec
julia <julia@Julia>
2019-05-29 17:31:30 +0100

    El programa con la matriz funcionando, falta conseguir que los componentes de fuera sean uno y los de dentro otro

--- src/newjavagit/Newjavagit.java
+++ src/newjavagit/Newjavagit.java
@@ -1,19 +1,41 @@
 package newjavagit;
 
+import java.util.Scanner;
+
 public class Newjavagit {
 
     public static void main(String[] args) {
         Newjavagit njg;
         njg = new Newjavagit();
         njg.crearMariz();
+        njg.imprimir();
     }
 
+    Scanner teclado;
+    int[][] matriz;
+    
     public void crearMariz() {
-        int matriz[][] = new int[3][3];
-        for (int x = 0; x < matriz.length; x++) {
-            for (int y = 0; y < matriz[x].length; y++) {
-                System.out.println(matriz[x][y]);
+        teclado = new Scanner(System.in);
+        System.out.print("Cuantas fila tiene la matriz:");
+        int filas = teclado.nextInt();
+        System.out.print("Cuantas columnas tiene la matriz:");
+        int columnas = teclado.nextInt();
+        matriz = new int[filas][columnas];
+        
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c < matriz[f].length; c++) {
+                System.out.print("Ingrese componente:");
+                matriz[f][c] = teclado.nextInt();
             }
+        }
+    }
+
+    public void imprimir() {
+        for(int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c < matriz[f].length; c++) {
+                System.out.print(matriz[f][c] + " ");
+            }
+            System.out.println();
         }
     }
 
