d5037c3871bffd0adadbf168afd0777718d7a75d
jcc <jcc@I7-JCC>
2019-06-04 22:21:47 +0100

    Archivo examen

---
+++ src/ut4_practico_2/Ut4_Practico_2.java
@@ -0,0 +1,40 @@
+package ut4_practico_2;
+
+/**
+ *
+ * @author jcc
+ */
+public class Ut4_Practico_2 {
+
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        sumar();
+        int rest1=10;
+        int rest2=20;
+        int rest3=30;
+        int restar = rest1-rest2-rest3;
+        System.out.println("Operación Restar"+restar);
+        int mul1=10;
+        int mul2=20;
+        int mul3=30;
+        int multiplicar = mul1*mul2*mul3;
+        System.out.println("Operación Restar"+restar);
+        int div1=10;
+        int div2=20;
+        int div3=30;
+        int dividir = div1/div2/div3;
+        System.out.println("Operación Restar"+restar);
+    }
+
+    public static void sumar() {
+        int sum1=10;
+        int sum2=20;
+        int sum3=30;
+        int sumar = sum1+sum2+sum3;
+        System.out.println("Operación Suma"+sumar);
+    }
+    
+}
